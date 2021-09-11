e5a35847d6203b1bb4607c0cd99f9fc7b15168f6
Jan Müller <janmueller3698@gmail.com>
2021-09-12 01:11:46 +0200

    ci(ci): add release job

--- release.config.js
+++ release.config.js
@@ -1,13 +1,15 @@
-export const plugins = [
-  '@semantic-release/commit-analyzer',
-  '@semantic-release/release-notes-generator',
-  '@semantic-release/changelog',
-  '@semantic-release/npm',
-  '@semantic-release/github',
-  [
-    '@semantic-release/git',
-    {
-      assets: ['CHANGELOG.md', './package.json'],
-    },
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', './package.json'],
+      },
+    ],
   ],
-]
+}
